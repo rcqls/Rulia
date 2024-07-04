d6c4407accadf96b9e09822571ac9d68796cc8ba
R cqls <rdrouilh@gmail.com>
2024-07-04 18:34:31 +0200

    add microbenchmark to README

--- inst/julia/RCallPtr.jl
+++ inst/julia/RCallPtr.jl
@@ -3,7 +3,7 @@ using RCall
 
 ## RCall with RObject forces the preservation of the R vectors 
 ## since the usual use case is to call R inside Julia
-## In oiur case, we don't want the R object garbage collected when the julia wrapper Ptr{Sxp} is garbage collected by julia
+## In our case, we don't want the R object garbage collected when the julia wrapper Ptr{Sxp} is garbage collected by julia
 reval_unprotected(str::T, env=RCall.Const.GlobalEnv) where T <: Union{AbstractString, Symbol} =
     RCall.unsafe_array(RCall.reval_p(RCall.rparse_p(str), sexp(env)))
 
