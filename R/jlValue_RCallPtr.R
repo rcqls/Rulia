26e9ab156a45582913cf4464fe0b37004fcd4079
R cqls <rdrouilh@gmail.com>
2024-07-09 08:47:20 +0200

    Add UnsafeCategoricalArray

--- R/jlValue_RCallPtr.R
+++ R/jlValue_RCallPtr.R
@@ -1,10 +1,10 @@
 ## To use UnsafeArray: call inisde R
 ## jlinclude("RCallPtr.jl",package="Rulia")
 
-toR.double <- toR.integer <- toR.logical <- toR.character <-function(obj, envir = globalenv()) {
+toR.double <- toR.integer <- toR.logical <- toR.complex <-function(obj, envir = globalenv()) {
     symb <- deparse(substitute(obj))
     #jlval <- jlcall("RCall.unsafe_array",jlcall("getfield", jlcall("reval", jlsymbol(symb)), jlsymbol("p")))
-    jlval <- jlcall("RCallPtr.reval_unprotected", jlsymbol(symb))
+    jlval <- jlcall("RCallPtr.reval_unprotected_vector", jlsymbol(symb))
     class(jlval) <- c("UnsafeArray", class(jlval))
     jlval
 }
@@ -17,4 +17,21 @@ toR.double <- toR.integer <- toR.logical <- toR.character <-function(obj, envir 
     jlcall("setindex!", jlval, value, as.integer(index))
     jlval
 }
+
+toR.factor  <-function(obj, envir = globalenv()) {
+    symb <- deparse(substitute(obj))
+    #jlval <- jlcall("RCall.unsafe_array",jlcall("getfield", jlcall("reval", jlsymbol(symb)), jlsymbol("p")))
+    jlval <- jlcall("RCallPtr.reval_unprotected_factor", jlsymbol(symb))
+    class(jlval) <- c("UnsafeCategoricalArray", "UnsafeArray", class(jlval))
+    jlval
+}
+
+# `[.UnsafeCategoricalArray` <- function(jlval, index) {
+#     jlcall("getindex", jlval, as.integer(index))
+# }
+
+# `[<-.UnsafeCategoricalArray` <- function(jlval, index, value) {
+#     jlcall("setindex!", jlval, value, as.integer(index))
+#     jlval
+# }
 
--- inst/julia/RCallPtr.jl
+++ inst/julia/RCallPtr.jl
@@ -1,10 +1,21 @@
 module RCallPtr
 using RCall
+using CategoricalArrays
 
 ## RCall with RObject forces the preservation of the R vectors 
 ## since the usual use case is to call R inside Julia
 ## In our case, we don't want the R object garbage collected when the julia wrapper Ptr{Sxp} is garbage collected by julia
-reval_unprotected(str::T, env=RCall.Const.GlobalEnv) where T <: Union{AbstractString, Symbol} =
-    RCall.unsafe_array(RCall.reval_p(RCall.rparse_p(str), sexp(env)))
+reval_unprotected_vector(str::T, env=RCall.Const.GlobalEnv) where T <: Union{AbstractString, Symbol} = RCall.unsafe_array(RCall.reval_p(RCall.rparse_p(str), sexp(env)))
+
+## s is here a factor and no Na allowed
+function unsafe_categorical(s::Ptr{IntSxp})
+    RCall.isFactor(s) || error("s is not an R factor")
+    refs = RCall.unsafe_array(s)
+    levels = RCall.rcopy(Array{String}, RCall.getattrib(s,RCall.Const.LevelsSymbol))
+    pool = CategoricalPool{String, Int32}(levels, RCall.isOrdered(s))
+    CategoricalArray{String, 1}(refs, pool)
+end
+
+reval_unprotected_factor(str::T, env=RCall.Const.GlobalEnv) where T <: Union{AbstractString, Symbol} = unsafe_categorical(RCall.reval_p(RCall.rparse_p(str), sexp(env)))
 
 end
